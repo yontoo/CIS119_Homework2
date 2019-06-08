154c4e622f26ef3adad1db5fb5d5346ab52a2072
yontoo <traylee1513@yahoo.com>
2019-06-07 18:11:19 -0700

    resume and style sheet for it

---
+++ resume.php
@@ -0,0 +1,43 @@
+<html lang="en">
+<head>
+<link rel="stylesheet" type="text/css" href="style.css" />
+<link href="https://fonts.googleapis.com/css?family=EB+Garamond&display=swap" rel="stylesheet"> 
+<title>My resume</title>
+</head>
+<body>
+    <h2 id="name">Timmy Ray Harris</h2>
+    <p>Sparks, NV <br />traylee1513@yahoo.com</p>
+
+    <h3>Work Experience</h3>
+    <hr>
+    <h3>Floor Staff</h3>
+    <p>Galaxy Theatres - Sparks, NV<br> November 2018 to April 2019</p>
+    <ul >
+    <li>Maintain theatre, restroom and lobby cleanliness.</li>
+    <li>Provide quality support to guests.</li>
+    <li>Take guests tickets and direct them to their theatre.</li>
+    <li>Large crowd control.</li>
+    <li>Stocking, cleaning, sweeping and mopping the kitchen.</li>
+    </ul>
+    <h3>Volunteer</h3>
+    <p>Boys & Girls Clubs of America - Fort Collins, CO<br> August 2011 to July 2012</p>
+    <ul>
+    <li>President of Torch Club.</li>
+    <li>Volunteered at various events such as town tree plantings and festivals like Kites in the Park.</li>
+    </ul>
+    <h3>Volunteer</h3>
+    <p>Poudre River Public Library District - Fort Collins, CO<br> June 2009 to August 2011</p>
+    <ul>
+    <li>Clean areas around the library that are dirty.</li>
+    <li>Re-shelving books.</li>
+    <li>Cleaning books.</li>
+    <li>Reading to children.</li>
+    <li>Making library mascot appearences.</li>
+    <li>Take head counts and  help set up at library events.</li>
+    </ul>
+
+    <h2>Education</h2>
+    <hr>
+    <h3>Highschool or equivalent.</h3>
+</body>
+</html>
